30904a63acf05708c84b50b9722bf0b77b4096a9
Matheus <msalvernaz13@gmail.com>
2025-04-02 17:13:58 -0300

    add buscador de linhas csv

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //efetuar login
+        Scanner scanner = new Scanner(System.in);
+        //informar matrícula
+        System.out.println("Para criação do UFFmail, informe sua matrícula:");
+        String numMatricula = scanner.nextLine();
+        System.out.println(numMatricula);
 
         //autenticar
+        ArrayList<String> dados = new ArrayList<>();
+        dados = ManipuladorCSV.dadosAluno(numMatricula);
+        for(String campo : dados){
+            System.out.println(campo);
+        }
 
         //verificar regularidade
 
--- src/ManipuladorCSV.java
+++ src/ManipuladorCSV.java
@@ -1,6 +1,40 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ManipuladorCSV {
+    private static final String PATH_ALUNOS = "src/alunos.csv";
+
+    //arraylist buscador de informações
+    public static ArrayList<String> dadosAluno(String matricula){
+        ArrayList<String> dados = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(PATH_ALUNOS));
+            String linha;
+            while((linha = br.readLine()) != null) {
+                String[] campos = linha.split(",");
+                if(campos[1].equals(matricula)){
+                    dados.addAll(Arrays.asList(campos));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return dados;
+    };
+
+    public static boolean verificarRegularidade(ArrayList<String> dadosAluno){
+        //verifica status
+        //get(index) pela possibilidade de mudança na ordem dos campos no aluno.csv
+        if(dadosAluno.get(5) == "Inativo"){
+            return false;
+        }
+        if(dadosAluno.get(4) == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
 
 }
