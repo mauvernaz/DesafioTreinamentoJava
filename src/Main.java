7e03949e155c1e27fca4564b0cb5bad7119d3173
Matheus <msalvernaz13@gmail.com>
2025-04-02 16:17:02 -0300

    add classes geradoruffmail e manipulador csv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        //efetuar login
 
+        //autenticar
+
+        //verificar regularidade
+
+        //informar inatividade
+        //informar existencia
+
+        //gerar opções uffmail
+
+        //escolher opção uffmail
+
+        //criar uffmail
+
+        //confirmar criação uffmail
+
+        //"enviar sms"
     }
 }
