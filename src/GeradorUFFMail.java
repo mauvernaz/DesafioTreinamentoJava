c727c6f5955e659461f640892a7d349f59774bb0
Matheus Alvernaz <msalvernaz13@gmail.com>
2025-04-08 12:04:51 -0300

    correção método gerarUFFMail

--- src/GeradorUFFMail.java
+++ src/GeradorUFFMail.java
@@ -7,14 +7,21 @@ public class GeradorUFFMail {
         String[] nomeSeparado = nomeCompleto.toLowerCase().split(" ");
 
         String nomePrimeiro = nomeSeparado[0];
-        String nomeMeio = nomeSeparado[1]; //considerando que sempre serão três nomes
+        //String nomeMeio = nomeSeparado[1]; //considerando que sempre serão três nomes
         String nomeUltimo = nomeSeparado[2];
 
         opcoesUFFmail.add(nomePrimeiro + "_" + nomeUltimo + "@id.uff.br");
-        opcoesUFFmail.add(nomePrimeiro + nomeMeio.charAt(0) + nomeUltimo.charAt(0) + "@id.uff.br");
         opcoesUFFmail.add(nomePrimeiro + nomeUltimo + "@id.uff.br");
         opcoesUFFmail.add(nomePrimeiro.charAt(0) + nomeUltimo + "@id.uff.br");
-        opcoesUFFmail.add(nomePrimeiro.charAt(0) + nomeMeio.charAt(0) + nomeUltimo + "@id.uff.br");
+        if(nomeSeparado.length > 2) {
+            String iniciaisMeio = "";
+            for (int i = 1; i < nomeSeparado.length - 1; i++) {
+                iniciaisMeio += nomeSeparado[i].charAt(0);
+            }
+            opcoesUFFmail.add(nomePrimeiro + iniciaisMeio + nomeUltimo + "@id.uff.br");
+            opcoesUFFmail.add(nomePrimeiro.charAt(0) + iniciaisMeio + nomeUltimo + "@id.uff.br");
+        }
+
 
         return opcoesUFFmail;
     }
